52eef1b49d1cfc0d868f8cdf58d13cb8093231e9
SeanHersh <32366781+SeanHersh@users.noreply.github.com>
2024-04-16 16:50:00 -0400

    Add files via upload

---
+++ csvcontroller.js
@@ -0,0 +1,52 @@
+const bodyParser = require('body-parser');
+const Papa = require('papaparse'); // Assuming you intended to use Papaparse for CSV conversion
+
+class CSVController {
+    constructor(){}
+
+    createCsv(items) {
+        const csv = Papa.unparse(items, {
+            header: true,
+            encoding: 'UTF-16'
+        });
+        return csv; 
+    }
+}
+
+module.exports = CSVController; 
+
+
+
+// // I am assuming you meant to define a proper endpoint to get YouTube data.
+// app.get('/get-youtube-data', async (req, res) => {
+//     try {
+//         // Simulated response from YouTube API
+//         const response = { items: [
+//             { id: { vidId: '12345' }, snippet: { title: 'Video Title 1' } }, //
+//             { id: { vidId: '67890' }, snippet: { title: 'Video Title 2' } }
+//         ]};
+// // unsure if this is how you search for specific data
+//         const vidData = response.items.map(item => ({
+//             vidID: item.id.vidId,
+//             vidTitle: item.snippet.title
+//         }));
+
+//         const csv = Papa.unparse(vidData, {
+//             header: true
+//         });
+
+//         res.attachment('videos.csv');
+//         res.type('text/csv');
+//         res.send(csv);
+
+//         fs.writeFile('videos.csv', csv, (err) => {
+//             if (err) {
+//                 console.error('Cannot write to csv file', err);
+//                 return;
+//             }
+//             console.log('CSV saved');
+//         });
+//     } catch (error) {
+//         res.status(500).send(`Failed to process data: ${error.message}`);
+//     }
+// });
---
+++ ytcontroller.js
@@ -0,0 +1,52 @@
+const {google} = require('googleapis');
+
+const API_KEY = "AIzaSyCBwhSvTPKw5qCcEVd4DccL8I7ReScGfYs";
+class YTController {
+    constructor() {
+        this.controller = google.youtube({
+            version: 'v3',
+            auth: API_KEY
+        });       
+        
+        this.curItems = ''; 
+    }
+
+    async searchResults(queryString) {
+        // Youtube query.
+        const res = await this.controller.search.list({
+            part: 'id, snippet', 
+            q: queryString,
+            maxResults: 50
+        });
+
+        const items = res.data.items; 
+        // console.log(items);
+
+        // Parse the results and construct an object with {videoId and videoTitle}
+        const data = [];
+        items.forEach(i => {
+            // Filter out only youtube videos. If we don't want to filter that out, then
+            // just remove this condition.
+            if (i.id.kind === 'youtube#video') {
+                data.push({
+                    vId: i.id.videoId,
+                    vTitle: i.snippet.title
+                });
+            }
+        });
+
+        // Save the current items in memory, so we can use them for constructing 
+        // our csv file.
+        this.curItems = data; 
+    
+        return Promise.resolve({
+            maxResults: res.data.pageInfo.totalResults,
+            items: data
+        });
+    }
+};
+
+module.exports = YTController;
+
+
+
