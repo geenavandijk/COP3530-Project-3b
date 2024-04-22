10f2130ad23924bf73cd58ee8ece084939bc7a48
SeanHersh <32366781+SeanHersh@users.noreply.github.com>
2024-04-22 18:49:02 -0400

    Update csvcontroller.js

--- csvcontroller.js
+++ csvcontroller.js
@@ -17,36 +17,3 @@ module.exports = CSVController;
 
 
 
-// // TODO: define a proper endpoint to get YouTube data.
-// app.get('/get-youtube-data', async (req, res) => {
-//     try {
-//         // Simulated response from YouTube API
-//         const response = { items: [
-//             { id: { vidId: '12345' }, snippet: { title: 'Video Title 1' } }, //
-//             { id: { vidId: '67890' }, snippet: { title: 'Video Title 2' } }
-//         ]};
-// // unsure if this is how you search for specific data
-//         const vidData = response.items.map(item => ({
-//             vidID: item.id.vidId,
-//             vidTitle: item.snippet.title
-//         }));
-
-//         const csv = Papa.unparse(vidData, {
-//             header: true
-//         });
-
-//         res.attachment('videos.csv');
-//         res.type('text/csv');
-//         res.send(csv);
-
-//         fs.writeFile('videos.csv', csv, (err) => {
-//             if (err) {
-//                 console.error('Cannot write to csv file', err);
-//                 return;
-//             }
-//             console.log('CSV saved');
-//         });
-//     } catch (error) {
-//         res.status(500).send(`Failed to process data: ${error.message}`);
-//     }
-// });
